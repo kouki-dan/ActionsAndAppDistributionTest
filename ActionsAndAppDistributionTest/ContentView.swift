80e6b84d1320fdfe1a963150b83fe15ba5ce0fdd
Kouki Saito <dan.addr.skd@gmail.com>
2019-09-26 21:19:16 +0900

    Initialize Xcode Project

---
+++ ActionsAndAppDistributionTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActionsAndAppDistributionTest
+//
+//  Created by Kouki Saito on 2019/09/26.
+//  Copyright © 2019 Kouki Saito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
